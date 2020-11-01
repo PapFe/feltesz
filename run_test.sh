64723c76eaf6c9eb8951fb53c5a74dc062995f15
PapFe <43848351+PapFe@users.noreply.github.com>
2020-11-01 19:21:27 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 IFS=$'\n'
-
-
 if test -f out_res.txt; do
 	rm out_res.txt
 done
-
-
-for i in 'cat inputs.txt'; do
-	echo "$i" | main >> out_res.txt
-done
-
-
-
+echo `./main unit1.json unit2.json` >> $2
+echo `./main unit1.json unit3.json` >> $2
+echo `./main unit2.json unit1.json` >> $2
+echo `./main unit2.json unit3.json` >> $2
+echo `./main unit3.json unit1.json` >> $2
+echo `./main unit3.json unit2.json` >> $2
